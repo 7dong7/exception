d3f69a730c09c44869d8859cb79656650ecc698a
7dong7 <tmdkdl777@gmail.com>
2025-01-20 21:35:20 +0900

    api exception - api 예외 처리 ( produces = MediaType.APPLICATION_JSON_VALUE )

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,14 +1,21 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @Controller
-public class ErrorController {
+public class ErrorPageController {
 
     //RequestDispatcher 상수로 정의되어 있음
     public static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
@@ -33,6 +40,22 @@ public class ErrorController {
         printErrorInfo(request);
         return "error-page/500";
     }
+
+    // org.springframework.http.MediaType
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request,
+                                                               HttpServletResponse response) {
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
+
 
     // 에러 상태 메시지
     private void printErrorInfo(HttpServletRequest request) {
